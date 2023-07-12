91095c7ea51df261ab0016de71cf40d57c1a04be
Arturo Palau <arturo.palau@sonarsource.com>
2023-07-12 09:39:29 -0500

    adding s2711 bug

---
+++ src/s2711.py
@@ -0,0 +1,12 @@
+print("Hello, world!")
+
+yield "This should not be here."
+
+def calculate_sum(a, b):
+    return a + b
+
+result = calculate_sum(5, 10)
+print("Result:", result)
+
+return "This should not be here either."
+
